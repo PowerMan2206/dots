e6ab8e8a3c3b029578f9233936bbfb6acc856ab6
PowerMan2206 <powerman2206@pm.me>
2020-11-14 20:52:54 +0100

    Make the Polybar launch script kill an already running instance

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,6 +1,10 @@
 # I have no idea how this works, but basically 
 # it just runs Polybar on each of your monitors
 
+# kill running polybar
+pkill polybar
+
+# run new polybar
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1 | tac); do
     MONITOR=$m polybar --reload main -c ~/.config/polybar/config.ini &
