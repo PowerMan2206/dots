46a5bf6bf0a7296eb6325dc62e6668bbac65a91a
PowerMan2206 <powerman2206@pm.me>
2020-11-18 11:46:51 +0100

    Made it so that the bottom bar is only on the main monitor now

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,15 +1,13 @@
 # I have no idea how this works, but basically 
 # it just runs Polybar on each of your monitors
 
-# kill running polybar
+# kill running bars
 pkill polybar
 
-# run bottom polybar
-for m in $(xrandr --query | grep " connected" | cut -d" " -f1 | tac); do
-	MONITOR=$m polybar --reload main -c ~/.config/polybar/config-bottom.ini &
-done
+# run bottom bar
+polybar --reload main -c ~/.config/polybar/config-bottom.ini &
 
-# run top polybar
+# run top bar
 for m in $(xrandr --query | grep " connected" | cut -d" " -f1 | tac); do
 	MONITOR=$m polybar --reload main -c ~/.config/polybar/config-top.ini &
 done
