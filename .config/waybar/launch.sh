078187b4f8e8861bd48ce15ede80e9f2dd2aeabd
powerman2206 <powerman2206@pm.me>
2021-05-14 11:26:10 +0200

    Stopped using my yucky pipewire script

--- .config/waybar/launch.sh
+++ .config/waybar/launch.sh
@@ -2,9 +2,7 @@
 
 # kill running bars and other stuff
 pkill -9 waybar
-pkill -9 pipewire-contro # not a typo, it's like this for some reason
 pkill -9 mpdvol
-pkill -9 pactl
 pkill -9 mpc
 
 #run bars
