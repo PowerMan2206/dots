c97a825a15da180ea12e8d972cb23effc8b45047
PowerMan2206 <powerman2206@pm.me>
2023-05-12 23:40:31 +0200

    Rewrote the lf config, it's an actual config now. The way of nuking the default binds is still kinda Bosanski but it's the way the dev plans to do it so ehh

--- .config/lf/lfcd.sh
+++ .config/lf/lfcd.sh
@@ -1,22 +1,9 @@
-# Change working dir in shell to last dir in lf on exit (adapted from ranger).
-#
-# You need to either copy the content of this file to your shell rc file
-# (e.g. ~/.bashrc) or source this file directly:
-#
-#     LFCD="/path/to/lfcd.sh"
-#     if [ -f "$LFCD" ]; then
-#         source "$LFCD"
-#     fi
-#
-# You may also like to assign a key to this command:
-#
-#     bind '"\C-o":"lfcd\C-m"'  # bash
-#     bindkey -s '^o' 'lfcd\n'  # zsh
-#
+# you gotta source this for it to work
+# makes it so that quitting lf drops you into the dir you were in in lf
 
 lfcd () {
     tmp="$(mktemp)"
-    command lf -last-dir-path="$tmp" "$@"
+    \lf -last-dir-path="$tmp" "$@"
     if [ -f "$tmp" ]; then
         dir="$(cat "$tmp")"
         \rm -f "$tmp"
