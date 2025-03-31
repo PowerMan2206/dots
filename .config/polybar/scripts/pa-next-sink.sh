98295a8af6b14c78c46192793b5b8fd53b126c11
PowerMan2206 <powerman2206@posteo.net>
2025-03-31 20:49:18 +0200

    Made volume module in Polybar scrollable and clickable on the sink name as well

--- .config/polybar/scripts/pa-next-sink.sh
+++ .config/polybar/scripts/pa-next-sink.sh
@@ -3,9 +3,6 @@
 # (C) 2022, @j2L4e, MIT License
 # https://gist.github.com/j2L4e/ae1042fc5968225fc3b6095d670fe273
 
-# for current-sink.sh
-touch /tmp/current-sink
-
 currentline=$(pactl list short sinks | grep -n "$(pactl get-default-sink)" | cut -d: -f 1)
 lastline=$(pactl list short sinks | wc -l)
 nextline=$(($currentline % $lastline + 1))
@@ -16,3 +13,12 @@ pactl set-default-sink "$nextsink"
 for sinkinput in $(pactl list short sink-inputs | cut -f 1); do
   pactl move-sink-input "$sinkinput" "@DEFAULT_SINK@"
 done
+
+# -=- ORIGINAL SCRIPT DONE -=-
+
+
+# have the xob thing when changing output
+pamixer --get-volume > /tmp/xob-volume
+
+# for current-sink.sh
+touch /tmp/current-sink
